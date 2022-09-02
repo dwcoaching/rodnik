1b5f80caf7412ddac66d105ab42776b904c528f3
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2022-09-02 20:14:10 +0300

    add new storage link

--- config/filesystems.php
+++ config/filesystems.php
@@ -83,6 +83,7 @@ return [
     'links' => [
         public_path('storage') => storage_path('app/public'),
         public_path('photos') => storage_path('app/photos'),
+        public_path('tiles') => storage_path('app/public/tiles'),
     ],
 
 ];
