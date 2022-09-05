c03ac1496f1c4c4e4a01cde21db3d882a1134081
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2022-09-05 18:49:01 +0300

    fix a bug

--- resources/views/livewire/spring.blade.php
+++ resources/views/livewire/spring.blade.php
@@ -81,7 +81,7 @@
             window.rodnikMap.showFeature({{ $springId }});
         }
 
-        if ({{ intval($userId)}}) {
+        if ({{ $user }}) {
             window.rodnikMap.locateWorld();
         }
 
