eb835efe393f9696ecb07b7dd32bd7a93621f2d5
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2022-10-03 17:03:09 +0300

    publish jetstream views

---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,16 @@
+<a href="/">
+    <svg class="w-16 h-16" width="387px" height="380px" viewBox="0 0 387 380" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <!-- Generator: Sketch 57.1 (83088) - https://sketch.com -->
+        <title>Combined Shape 2</title>
+        <desc>Created with Sketch.</desc>
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="Artboard" transform="translate(-872.000000, -879.000000)" fill="#67BFFF" fill-rule="nonzero">
+                <g id="Group" transform="translate(538.000000, 251.000000)">
+                    <g id="Combined-Shape-2" transform="translate(334.000000, 628.000000)">
+                        <path d="M193.5,0 C300.367099,0 387,85.0658975 387,190 C387,294.934102 300.367099,380 193.5,380 C86.6329009,380 0,294.934102 0,190 C0,85.0658975 86.6329009,0 193.5,0 Z M194.716981,102.307692 C146.324332,102.307692 107.09434,141.023582 107.09434,188.782051 C107.09434,236.540521 146.324332,275.25641 194.716981,275.25641 C243.10963,275.25641 282.339623,236.540521 282.339623,188.782051 C282.339623,141.023582 243.10963,102.307692 194.716981,102.307692 Z" id="Combined-Shape"></path>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </svg>
+</a>
