8a4accc65d371aabcf8fe2eaa02436c7332fcaae
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2022-10-15 18:06:44 +0200

    prevent select

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
                             <path d="M13.28 7.78l3.22-3.22v2.69a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.69l-3.22 3.22a.75.75 0 001.06 1.06zM2 17.25v-4.5a.75.75 0 011.5 0v2.69l3.22-3.22a.75.75 0 011.06 1.06L4.56 16.5h2.69a.75.75 0 010 1.5h-4.5a.747.747 0 01-.75-.75zM12.22 13.28l3.22 3.22h-2.69a.75.75 0 000 1.5h4.5a.747.747 0 00.75-.75v-4.5a.75.75 0 00-1.5 0v2.69l-3.22-3.22a.75.75 0 10-1.06 1.06zM3.5 4.56l3.22 3.22a.75.75 0 001.06-1.06L4.56 3.5h2.69a.75.75 0 000-1.5h-4.5a.75.75 0 00-.75.75v4.5a.75.75 0 001.5 0V4.56z" />
                         </svg>
                     </div>
-                    <div x-cloak x-show="fullscreen" class="">
+                    <div x-cloak x-show="fullscreen" class="select-none">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                             <path d="M3.28 2.22a.75.75 0 00-1.06 1.06L5.44 6.5H2.75a.75.75 0 000 1.5h4.5A.75.75 0 008 7.25v-4.5a.75.75 0 00-1.5 0v2.69L3.28 2.22zM13.5 2.75a.75.75 0 00-1.5 0v4.5c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-2.69l3.22-3.22a.75.75 0 00-1.06-1.06L13.5 5.44V2.75zM3.28 17.78l3.22-3.22v2.69a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.69l-3.22 3.22a.75.75 0 101.06 1.06zM13.5 14.56l3.22 3.22a.75.75 0 101.06-1.06l-3.22-3.22h2.69a.75.75 0 000-1.5h-4.5a.75.75 0 00-.75.75v4.5a.75.75 0 001.5 0v-2.69z" />
                         </svg>
@@ -33,7 +33,7 @@
                 </div>
                 <div class="relative">
                     <div @click="filtersOpen = ! filtersOpen;"
-                        class="border border-2 mt-2 h-9 w-9 bg-white shadow-sm rounded-md cursor-pointer flex items-center justify-center"
+                        class="select-none border border-2 mt-2 h-9 w-9 bg-white shadow-sm rounded-md cursor-pointer flex items-center justify-center"
                         :class="{
                             'border-blue-600': filtersOpen,
                             'border-white': ! filtersOpen,
@@ -173,7 +173,7 @@
                         }"
                         @click="layersOpen = ! layersOpen;"
                         @click.outside="layersOpen = false;"
-                        class="border border-2 mt-2 h-9 w-9 bg-white overflow:hidden shadow-sm rounded-md cursor-pointer flex items-center justify-center"
+                        class="select-none border border-2 mt-2 h-9 w-9 bg-white overflow:hidden shadow-sm rounded-md cursor-pointer flex items-center justify-center"
                         :class="{
                                 'border-blue-600': layersOpen,
                                 'border-white': ! layersOpen,
@@ -277,7 +277,7 @@
             </div>
         </div>
         <div class="w-full sm:w-1/2 sm:h-full h-1/2 overflow-y-scroll px-6">
-             @include('navbar')
+            @include('navbar')
             <div class="">
                 <livewire:spring spring_id="{{ $springId }}" user_id="{{ $userId }}" />
             </div>
