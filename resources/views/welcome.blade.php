cbbba628f749b4eee8f3a2db9886f660ca9296e9
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2023-05-07 21:52:13 +0200

    put back

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,6 @@
             }"
             x-init="
                 initOpenLayers($el.id);
-                window.scrollTo(0, document.body.scrollHeight + 500);
                 window.rodnikMap.springsSource({{ intval($userId) }});
             ">
             <div class="absolute top-2 right-2" style="z-index: 10000;">
