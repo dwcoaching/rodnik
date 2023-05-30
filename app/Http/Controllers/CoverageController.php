7e76989ee0b4bfa142838a0b644b5703111dbd38
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2023-05-30 23:53:27 +0300

    remove models and use less memory\

--- app/Http/Controllers/CoverageController.php
+++ app/Http/Controllers/CoverageController.php
@@ -5,12 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\OverpassBatch;
 use App\Models\OverpassCheck;
+use Illuminate\Support\Facades\DB;
 
 class CoverageController extends Controller
 {
     public function index(OverpassBatch $overpassBatch)
     {
-        $overpassChecks = $overpassBatch->overpassChecks()
+        // $overpassChecks = $overpassBatch->overpassChecks()
+        //     ->orderBy('longitude_from')
+        //     ->orderBy('latitude_from', 'desc')
+        //     ->get();
+
+        $overpassChecks = DB::table('overpass_checks')
+            ->select([
+                'latitude_from',
+                'latitude_to',
+                'longitude_from',
+                'longitude_to',
+                'covered_by',
+            ])
+            ->where('overpass_batch_id', $overpassBatch->id)
             ->orderBy('longitude_from')
             ->orderBy('latitude_from', 'desc')
             ->get();
