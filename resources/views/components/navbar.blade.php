5e32e0262748dfdc43573912db776173ab47b754
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2023-10-08 19:07:45 +0300

    fix user icon

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -63,9 +63,11 @@
                 <x-jet-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                            <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                            </button>
+                            <div>
+                                <button class="flex text-sm rounded-full focus:outline-none transition">
+                                    <img class="h-7 w-7 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                </button>
+                            </div>
                         @else
                             <span class="inline-flex rounded-md">
                                 <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
