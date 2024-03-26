9d05eecc0b563eb852f542d804b9e1a003c1919c
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2024-03-26 23:14:16 +0200

    paint POIs also

--- resources/js/styles/track.js
+++ resources/js/styles/track.js
@@ -1,8 +1,15 @@
-import { Stroke, Style } from 'ol/style'
+import { Stroke, Style, Circle, Fill } from 'ol/style'
 
 export default new Style({
     stroke: new Stroke({
         color: '#000000',
         width: 2,
-     })
+
+    }),
+    image: new Circle({
+        radius: 2,
+        fill: new Fill({
+            color: '#000000',
+        }),
+    })
 })
