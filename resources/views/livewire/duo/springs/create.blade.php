903b6da580e532f6c69de31bc6d8c08b71587a3a
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2024-09-14 17:00:24 +0300

    make locate by photo droppable

--- resources/views/livewire/duo/springs/create.blade.php
+++ resources/views/livewire/duo/springs/create.blade.php
@@ -191,13 +191,13 @@
                                 event.target.value = ''
                             }
                         }"
-                        class="btn btn-sm">
+                        class="btn btn-sm relative">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
                             <path d="M9.5 8.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
                             <path fill-rule="evenodd" d="M2.5 5A1.5 1.5 0 0 0 1 6.5v5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5v-5A1.5 1.5 0 0 0 13.5 5h-.879a1.5 1.5 0 0 1-1.06-.44l-1.122-1.12A1.5 1.5 0 0 0 9.38 3H6.62a1.5 1.5 0 0 0-1.06.44L4.439 4.56A1.5 1.5 0 0 1 3.38 5H2.5ZM11 8.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" clip-rule="evenodd" />
                         </svg>
                         Locate by Photo
-                        <input x-on:change="handlePhotoSelect($event)" id="locate-by-photo" name="locate-by-photo" type="file" class="sr-only">
+                        <input class="opacity-0 absolute inset-0" x-on:change="handlePhotoSelect($event)" id="locate-by-photo" name="locate-by-photo" type="file" class="sr-only">
                     </label>
                 </div>
 
