e9bd17257be7fb5ef98ee342679cf0ececeb348f
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2025-02-24 14:22:38 +0200

    add lycian way

--- app/Http/Controllers/Api/V1/ExportedAreasController.php
+++ app/Http/Controllers/Api/V1/ExportedAreasController.php
@@ -11,28 +11,47 @@ class ExportedAreasController extends Controller
 {
     public function show(string $area)
     {
-        if (! in_array($area, ['armenia', 'yerevan'])) {
+        if (! in_array($area, ['armenia', 'yerevan', 'lycian-way'])) {
             abort(404);
         }
 
-        $springs = Spring::where([
+        $armeniaBoundingBox = [
             ['latitude', '>=', 38.8],
             ['latitude', '<=', 41.4],
             ['longitude', '>=', 43.4],
             ['longitude', '<=', 46.7],
-        ])->with(['reports.photos'])->get();
+        ];
+
+        $lycianWayBoudingBox = [
+            ['latitude', '>=', 36],
+            ['latitude', '<=', 37],
+            ['longitude', '>=', 29],
+            ['longitude', '<=', 31],
+        ];
+
+        $boundingBox = match ($area) {
+            'armenia' => $armeniaBoundingBox,
+            'yerevan' => $armeniaBoundingBox,
+            'lycian-way' => $lycianWayBoudingBox,
+        };
+
+        $springs = Spring::where($boundingBox)->with(['reports.photos'])->get();
 
         if (extension_loaded('geos')) {
             $areafile = match($area) {
                 'armenia' => 'geojson/armenia.geojson',
                 'yerevan' => 'geojson/yerevan.geojson',
+                'lycian-way' => null,
             };
-            $area = \geoPHP::load(file_get_contents(resource_path($areafile)), 'json');
 
-            $springs = $springs->filter(function ($spring) use ($area) {
-                $point = \geoPHP::load('POINT('.$spring->longitude.' '.$spring->latitude.')', 'wkt');
-                return $area->contains($point);
-            });
+            if ($areafile) {
+                $area = \geoPHP::load(file_get_contents(resource_path($areafile)), 'json');
+
+                $springs = $springs->filter(function ($spring) use ($area) {
+                    $point = \geoPHP::load('POINT('.$spring->longitude.' '.$spring->latitude.')', 'wkt');
+                    return $area->contains($point);
+                });
+            }
         }
 
         return ExportedSpringResource::collection($springs)->toJson(
