64a0a07d468debe999d2012aeb36f36955cea010
Andrew Kolpakov <kolpakov@dwcoaching.ru>
2025-04-09 21:49:27 +0300

    fix the test

--- tests/Feature/OverpassTest.php
+++ tests/Feature/OverpassTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Library\Overpass;
+use App\Models\Spring;
 use App\Models\SpringRevision;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -9,9 +10,13 @@ uses(RefreshDatabase::class);
 test('spring revision is created', function () {
     $json = file_get_contents(base_path('tests/stubs/overpass.json'));
 
-    Overpass::parse(json_decode($json));
+    $result = Overpass::parse(json_decode($json));
 
-    $springRevision = SpringRevision::where('spring_id', 1)
+    // Find the spring with the OSM node ID from our test data
+    $spring = Spring::where('osm_node_id', 7600556407)->first();
+    
+    // Get the revision for that spring
+    $springRevision = SpringRevision::where('spring_id', $spring->id)
         ->orderBy('id', 'desc')->first();
 
     expect($springRevision)->not->toBeNull();
